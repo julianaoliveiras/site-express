b230981e65972d2049810339b480e9f404b47a9e
Juliana Oliveira <h4bituei.o@gmail.com>
2021-11-19 19:04:00 -0300

    front das páginas finalizadas

--- receitas-project/app.js
+++ receitas-project/app.js
@@ -1,31 +1,35 @@
-var createError = require('http-errors');
+var createError = require('http-errors'); 
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var logger = require('morgan'); 
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require('./routes/index'); 
+var usersRouter = require('./routes/users');  
 
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-app.use(logger('dev'));
+var app = express();         
+     
+// view engine setup  
+var mustacheExpress = require("mustache-express");      
+var engine = mustacheExpress();   
+app.engine("mustache", engine);       
+    
+app.set('views', path.join(__dirname, 'views')); 
+app.set('view engine', 'mustache');   
+ 
+app.use(logger('dev'));  
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser()); 
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(express.static(__dirname + '/public')); 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/users', usersRouter); 
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
-});
+}); 
 
 // error handler
 app.use(function(err, req, res, next) {
@@ -39,3 +43,4 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+
--- receitas-project/routes/index.js
+++ receitas-project/routes/index.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
+//var home = require('../views/home/home.mustache')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('./home', { title: 'Express' });
+});
+
+router.get('/contato', function(req, res, next) {
+  res.render('./contato', { title: 'Express' });
+});
+router.get('/tecnologias', function(req, res, next) {
+  res.render('./tecnologias', { title: 'Express' });
+});
+router.get('/sobre', function(req, res, next) {
+  res.render('./sobre', { title: 'Express' });
+});
+router.get('/login', function(req, res, next) {
+  res.render('./login', { title: 'Express' });
+});
+router.get('/registrar', function(req, res, next) {
+  res.render('./registrar', { title: 'Express' });
 });
 
 module.exports = router;
