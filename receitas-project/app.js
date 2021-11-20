46ad1079436d89464b26e8ce33a1d5ae4ce0d178
Cassio Pereira <cassiogomes00@gmail.com>
2021-11-20 16:22:18 -0300

    Adaptar o metodo de autenticacao de login para usar as informacoes do arquivo de configuracao

--- receitas-project/app.js
+++ receitas-project/app.js
@@ -1,13 +1,14 @@
-var createError = require('http-errors'); 
+var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
-var logger = require('morgan'); 
+var logger = require('morgan');
 var passport = require('passport');
 var session = require('express-session');
+require('dotenv').config();
 
-var indexRouter = require('./routes/index'); 
-var usersRouter = require('./routes/users');  
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 var loginRouter = require('./routes/login');
 
 function authenticationMiddleware(req, res, next) {
@@ -15,47 +16,48 @@ function authenticationMiddleware(req, res, next) {
   res.redirect('/login?fail=true');
 }
 
-var app = express();         
+var app = express();
 
-// view engine setup  
-var mustacheExpress = require("mustache-express");      
-var engine = mustacheExpress();   
-app.engine("mustache", engine);       
-    
-app.set('views', path.join(__dirname, 'views')); 
-app.set('view engine', 'mustache');   
- 
-app.use(logger('dev'));  
+// view engine setup
+var mustacheExpress = require('mustache-express');
+var engine = mustacheExpress();
+app.engine('mustache', engine);
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'mustache');
+
+app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser()); 
+app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(__dirname + '/public')); 
+app.use(express.static(__dirname + '/public'));
 app.use('/', indexRouter);
-app.use('/users', usersRouter); 
- 
-require('./auth')(passport); 
-app.use(session({  
-  secret: '123',//configure um segredo seu aqui,
-  resave: false,
-  saveUninitialized: false, 
-  cookie: { maxAge: 30 * 60 * 1000 }//30min
-}))
+app.use('/users', usersRouter);
+
+require('./auth')(passport);
+app.use(
+  session({
+    secret: '123', //configure um segredo seu aqui,
+    resave: false,
+    saveUninitialized: false,
+    cookie: { maxAge: 30 * 60 * 1000 }, //30min
+  })
+);
 app.use(passport.initialize());
 app.use(passport.session());
 
 app.use('/login', loginRouter);
 app.use('/users', authenticationMiddleware, usersRouter);
-app.use('/', authenticationMiddleware,  indexRouter);
-
+app.use('/', authenticationMiddleware, indexRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
-}); 
+});
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -66,4 +68,3 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
-
--- receitas-project/auth.js
+++ receitas-project/auth.js
@@ -1,54 +1,67 @@
 const bcrypt = require('bcryptjs');
 const LocalStrategy = require('passport-local').Strategy;
-const users = [{
+const users = [
+  {
     _id: 1,
-    username: "adm",
-    password: "$2a$06$HT.EmXYUUhNo3UQMl9APmeC0SwoGsx7FtMoAWdzGicZJ4wR1J8alW",
-    email: "teste@gmail.com"
-}];
+    username: process.env.USER_EMAIL.split('@')[0],
+    password: process.env.USER_PASSWORD,
+    email: process.env.USER_EMAIL,
+  },
+];
 
 module.exports = function (passport) {
+  function findUser(username) {
+    return users.find((user) => user.username === username);
+  }
 
-    function findUser(username) {
-        return users.find(user => user.username === username);
-    }
+  function findUserById(id) {
+    return users.find((user) => user._id === id);
+  }
 
-    function findUserById(id) {
-        return users.find(user => user._id === id);
-    }
-
-    passport.serializeUser((user, done) => {
-        done(null, user._id);
+  function findUserByEmail(email) {
+    return users.find((user) => {
+      return user.email === email;
     });
+  }
 
-    passport.deserializeUser((id, done) => {
-        try {
-            const user = findUserById(id);
-            done(null, user);
-        } catch (err) {
-            done(err, null);
-        }
-    });
+  passport.serializeUser((user, done) => {
+    done(null, user._id);
+  });
 
-    passport.use(new LocalStrategy({
+  passport.deserializeUser((id, done) => {
+    try {
+      const user = findUserById(id);
+      done(null, user);
+    } catch (err) {
+      done(err, null);
+    }
+  });
+
+  passport.use(
+    new LocalStrategy(
+      {
         usernameField: 'username',
-        passwordField: 'password'
-    },
-        (username, password, done) => {
-            try {
-                const user = findUser(username);
+        passwordField: 'pass',
+      },
+      (username, password, done) => {
+        try {
+          const user = findUserByEmail(username);
 
-                // usuário inexistente
-                if (!user) { return done(null, false) }
+          // usuário inexistente
+          if (!user) {
+            return done(null, false);
+          }
 
-                // comparando as senhas
-                const isValid = bcrypt.compareSync(password, user.password);
-                if (!isValid) return done(null, false)
-                
-                return done(null, user)
-            } catch (err) {
-                done(err, false);
-            }
+          // comparando as senhas
+          // const isValid = bcrypt.compareSync(password, user.password);
+          const isValid = password === user.password;
+          if (!isValid) return done(null, false);
+
+          return done(null, user);
+        } catch (err) {
+          done(err, false);
         }
-    ));
-}
+      }
+    )
+  );
+};
