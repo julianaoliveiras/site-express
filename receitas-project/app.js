9a2ef5f2d0292eb48be48de01c3796e7d538fc33
Juliana Oliveira <h4bituei.o@gmail.com>
2021-11-21 20:09:15 -0300

    atualizando pagina contato

--- receitas-project/app.js
+++ receitas-project/app.js
@@ -125,13 +125,13 @@ app.post('/send', (req, res) => {
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-  next(createHttpError(404)); 
+  next(createHttpError(404));   
 });
 
 // error handler
-app.use(function (err, req, res, next) {
+app.use(function (err, req, res, next) { 
   // set locals, only providing error in development
-  res.locals.message = err.message;
+  res.locals.message = err.message;  
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
